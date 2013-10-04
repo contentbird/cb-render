efbff476e00aaf527536e84f484bb1e2e6f4d78f
nna <nico.nardone@gmail.com>
2013-10-04 15:39:22 +0200

    gitignore

--- lib/cb-render.rb
+++ lib/cb-render.rb
@@ -1,35 +1,37 @@
 require 'cb-render/content_helper'
-module CB::Render
-  class FrameworkNotFound < StandardError; end
+module CB
+  module Render
+    class FrameworkNotFound < StandardError; end
 
-  # Inspired by Kaminari
-  def self.load!
+    # Inspired by Kaminari
+    def self.load!
 
-    if rails?
-      require 'sass-rails'
-      register_rails_engine
+      if rails?
+        require 'sass-rails'
+        register_rails_engine
+      end
+
+      if !(rails?)
+        raise CB::Render::FrameworkNotFound, "cb-render requires Rails > 4 which is not loaded"
+      end
+
+      stylesheets = File.expand_path(File.join("..", 'vendor', 'assets', 'stylesheets'))
+      ::Sass.load_paths << stylesheets
     end
 
-    if !(rails?)
-      raise CB::Render::FrameworkNotFound, "cb-render requires Rails > 4 which is not loaded"
+    private
+
+    def self.asset_pipeline?
+      defined?(::Sprockets)
     end
 
-    stylesheets = File.expand_path(File.join("..", 'vendor', 'assets', 'stylesheets'))
-    ::Sass.load_paths << stylesheets
-  end
+    def self.rails?
+      defined?(::Rails)
+    end
 
-  private
-
-  def self.asset_pipeline?
-    defined?(::Sprockets)
-  end
-
-  def self.rails?
-    defined?(::Rails)
-  end
-
-  def self.register_rails_engine
-    require 'cb-render/engine'
+    def self.register_rails_engine
+      require 'cb-render/engine'
+    end
   end
 end
 
