65ac3ae2af1462fde1c8704f9f69644b45c4d2c0
nna <nico.nardone@gmail.com>
2014-01-07 16:57:28 +0100

    use bower as js lib manager and start with rainbow lib

--- vendor/assets/javascripts/cb-render.js
+++ vendor/assets/javascripts/cb-render.js
@@ -1,1 +1,4 @@
-//= require cb-render/rainbow-custom.min
+// require cb-render/rainbow-custom.min
+//= require cb-render/rainbow/js/rainbow.min.js
+//= require_tree ./cb-render/rainbow/js/language
+// require cb-render/webshim/js-webshim/minified/polyfiller.js
