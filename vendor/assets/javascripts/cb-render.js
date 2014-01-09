0c19a6d1b1615b1cb7ae2b4c4379a60aa07b423b
nna <nico.nardone@gmail.com>
2014-01-09 15:27:42 +0100

    add H5F and remove webshims
    also remove old rainbow js files

--- vendor/assets/javascripts/cb-render.js
+++ vendor/assets/javascripts/cb-render.js
@@ -1,4 +1,3 @@
-// require cb-render/rainbow-custom.min
 //= require cb-render/rainbow/js/rainbow.min.js
 //= require_tree ./cb-render/rainbow/js/language
-// require cb-render/webshim/js-webshim/minified/polyfiller.js
+//= require cb-render/H5F/h5f.min.js
