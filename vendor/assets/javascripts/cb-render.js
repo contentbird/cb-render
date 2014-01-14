9f3e4d33e5f03be4340f23b6023fce46711d4f63
nna <nico.nardone@gmail.com>
2014-01-14 17:14:10 +0100

    added tmpl library and modal

--- vendor/assets/javascripts/cb-render.js
+++ vendor/assets/javascripts/cb-render.js
@@ -1,6 +1,7 @@
 //= require cb-render/rainbow/js/rainbow.min.js
 //= require_tree ./cb-render/rainbow/js/language
 //= require cb-render/H5F/h5f.min.js
+//= require cb-render/blueimp-tmpl/js/tmpl.js
 //= require cb-render/cb_base
 //= require cb-render/modal
 //= require cb-render/gallery
