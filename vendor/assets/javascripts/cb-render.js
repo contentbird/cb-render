0fcd0a0cf0b5ea01e6f54605e129284b2b035e26
nna <nico.nardone@gmail.com>
2014-01-14 16:56:46 +0100

    move cb_base.js to cb-render

--- vendor/assets/javascripts/cb-render.js
+++ vendor/assets/javascripts/cb-render.js
@@ -1,5 +1,6 @@
 //= require cb-render/rainbow/js/rainbow.min.js
 //= require_tree ./cb-render/rainbow/js/language
 //= require cb-render/H5F/h5f.min.js
+//= require cb-render/cb_base
 //= require cb-render/modal
 //= require cb-render/gallery
