2a8f178f7965128dfc7b5acf68bc2475078b42c5
nna <nico.nardone@gmail.com>
2014-01-27 18:14:51 +0100

    update font recompilation setup

--- lib/cb-render/engine.rb
+++ lib/cb-render/engine.rb
@@ -3,9 +3,9 @@ module CB
     module Rails
       class Engine < ::Rails::Engine
         initializer "cb-render.assets.precompile" do |app|
-          config.assets.paths << '../vendor/assets/fonts'
           config.assets.precompile += ["../vendor/assets/javascripts/cb_render.js"]
-          config.assets.precompile += %w( .svg .eot .woff .ttf )
+          config.assets.precompile += ["../vendor/assets/fonts/contentbird.svg", "../vendor/assets/fonts/contentbird.eot", "../vendor/assets/fonts/contentbird.woff", "../vendor/assets/fonts/contentbird.ttf"]
+          #config.assets.precompile += %w( .svg .eot .woff .ttf )
           #app.config.assets.precompile << %r(bootstrap/glyphicons-halflings-regular\.(?:eot|svg|ttf|woff)$)
         end
         initializer "cb-render.view_helpers" do
