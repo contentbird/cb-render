f3f88c014edd3440ff8aed04abb23bff5d85a042
nna <nico.nardone@gmail.com>
2014-01-27 17:22:58 +0100

    move fonts to cb-render

--- lib/cb-render/engine.rb
+++ lib/cb-render/engine.rb
@@ -3,7 +3,9 @@ module CB
     module Rails
       class Engine < ::Rails::Engine
         initializer "cb-render.assets.precompile" do |app|
+          config.assets.paths << '../vendor/assets/fonts'
           config.assets.precompile += ["../vendor/assets/javascripts/cb_render.js"]
+          config.assets.precompile += %w( .svg .eot .woff .ttf )
           #app.config.assets.precompile << %r(bootstrap/glyphicons-halflings-regular\.(?:eot|svg|ttf|woff)$)
         end
         initializer "cb-render.view_helpers" do
