3d2ff59bc140a7d504cc30e99275e0e95d7c0be1
nna <nico.nardone@gmail.com>
2014-01-28 10:33:51 +0100

    fix font asset path

--- lib/cb-render/engine.rb
+++ lib/cb-render/engine.rb
@@ -3,7 +3,7 @@ module CB
     module Rails
       class Engine < ::Rails::Engine
         initializer "cb-render.assets.precompile" do |app|
-          config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+          config.assets.paths << '../app/assets/fonts'
           config.assets.precompile += ["../vendor/assets/javascripts/cb_render.js"]
         end
         initializer "cb-render.view_helpers" do
