b129cee677347cc871775b172a6ed9578947bb3b
nna <nico.nardone@gmail.com>
2014-01-28 10:28:11 +0100

    move font asset path to cb-render

--- lib/cb-render/engine.rb
+++ lib/cb-render/engine.rb
@@ -3,8 +3,8 @@ module CB
     module Rails
       class Engine < ::Rails::Engine
         initializer "cb-render.assets.precompile" do |app|
+          config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
           config.assets.precompile += ["../vendor/assets/javascripts/cb_render.js"]
-          #app.config.assets.precompile << %r(bootstrap/glyphicons-halflings-regular\.(?:eot|svg|ttf|woff)$)
         end
         initializer "cb-render.view_helpers" do
           ActionView::Base.send :include, ContentHelper
